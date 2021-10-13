9dd23bcc629bf93838cd7c4b2a47a206b24b61e8
goldentechie <bendam1207@gmail.com>
2021-10-13 13:44:06 +0530

    added new logo

--- src/app/web3/provider/index.tsx
+++ src/app/web3/provider/index.tsx
@@ -69,7 +69,7 @@ const AppProviderProvider: React.FC<AppProviderProviderProps> = ({
     const _wallet = await connectWallet();
     setWallet(_wallet);
     const provider = new SolanaProvider(
-      new Connection(SolanaNetworks.DEV),
+      new Connection(SolanaNetworks.LOCAL),
       _wallet,
       {}
     );
